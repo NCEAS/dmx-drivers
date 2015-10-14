d64d9bc3ca907fc069013c9d73fe523b126e2f02
Rachael Blake <blake@nceas.ucsb.edu>
2015-10-14 14:44:01 -0700

    fixed function to be more general

--- Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/InvertsAlgae_DMX_Benthic.R
+++ Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/InvertsAlgae_DMX_Benthic.R
@@ -44,6 +44,7 @@ chiton <- c("Cryptochiton stelleri")
 clam <- c("Hiatella arctica","Mya truncata")
 coralline_alga <- c("Corallina sp.","encrusting coralline algae","foliose coralline algae")
 encrusting_red_alga <- c("Hildenbrandia sp.","non-coralline algal crust")
+#ephemeral_aglae <- (???????????????????????????????????????????)
 filamentous_brown <- c("Pylaiella littoralis")
 filamentous_green <- c("Chaetomorpha melagonium","Chaetomorpha sp.","Cladophora / Chaetomorpha sp.",
                        "Ulothrix flacca")
@@ -82,6 +83,7 @@ IA_GOA <- IA %>%
                               ifelse((Species_Name %in% clam),'clam',
                               ifelse((Species_Name %in% coralline_alga),'coralline_alga',
                               ifelse((Species_Name %in% encrusting_red_alga),'encrusting_red_alga',
+                              #ifelse((Species_Name %in% ephemeral_aglae),'ephemeral_aglae',
                               ifelse((Species_Name %in% filamentous_brown),'filamentous_brown',
                               ifelse((Species_Name %in% filamentous_green),'filamentous_green',
                               ifelse((Species_Name %in% green_alga),'green_alga',
@@ -91,7 +93,7 @@ IA_GOA <- IA %>%
                               ifelse((Species_Name %in% red_alga),'red_alga',
                               ifelse((Species_Name %in% sponge),'sponge',
                               ifelse((Species_Name %in% tunicate),'tunicate',
-                              ifelse((Species_Name %in% worm),'worm',""))))))))))))))))))
+                              ifelse((Species_Name %in% worm),'worm',"")))))))))))))))))))
                  )     # add new column with common category
 head(IA_GOA) ; IA_GOA[45:90,]
 
@@ -99,47 +101,51 @@ head(IA_GOA) ; IA_GOA[45:90,]
 ##### 
 # FUNCTION for getting Percent Cover for all intertidal inverts and algae
 PerCovCalc <- function(df, new_column_name) { 
-              df %>%
-              count(Site_Code, Site_Name, Year, Elevation_Position, Quadrat) %>%
-              mutate(Per_Cov = (n/25)*100) %>%   # calculates the percent cover per quadrat
-              group_by(Site_Name, Year, Quadrat) %>%
-              summarise_(.dots = setNames(list(~mean(Per_Cov)), new_column_name)) %>%
-              ungroup()
+              # calculate the total number of points used in each quadrat
+              x <- IA_GOA %>% 
+                   filter(Species_Name %in% c("Bare Substrate","bare substrate")) %>% 
+                   count(Site_Code, Site_Name, Year, Elevation_Position, Quadrat) %>%
+                   rename(Point_Count=n)
+              # join calculated points df to referenced df
+              df1 <- left_join(df, x, by=c("Site_Code", "Site_Name", "Year", 
+                                           "Elevation_Position", "Quadrat"))
+              # calculate percent cover per quadrat
+              df1 %>%
+                  count(Site_Code, Site_Name, Year, Elevation_Position, Quadrat, Point_Count) %>%
+                  mutate(Per_Cov = (n/Point_Count)*100) %>% 
+                  group_by(Site_Name, Year, Quadrat) %>%
+                  summarise_(.dots = setNames(list(~mean(Per_Cov)), new_column_name)) %>%  # mean of elevations together
+                  ungroup() %>% 
+                  select_("Site_Name", "Year", "Quadrat", new_column_name) %>%
+                  arrange(Site_Name, Year, Quadrat)
               }
 #####
 #####
 
-test <- IA_GOA %>%
-              count(Site_Code, Site_Name, Year, Elevation_Position, Quadrat) %>%
-              mutate(Per_Cov = (n/25)*100)
-
-
-
-
 # Bare Substrate 
 BS_IA_1 <- filter(IA_GOA, Species_Name=="Bare Substrate", Layer_Num=="1") # only in layer 1 according to Tom Dean
-BS_IA <- PerCovCalc(BS_IA_1, "Bare_Sub_Per_Cov")    ;   BS_IA
+BS_IA <- PerCovCalc(BS_IA_1, "Bare_Sub_Per_Cov")    
 
 # Barnacles
 b_IA_1 <- filter(IA_GOA, Common_Cat=="barnacle")   # Filter out the species/entry of interest
-b_IA <- PerCovCalc(b_IA_1, "barnacle_Per_Cov") ; b_IA  # call the function
+b_IA <- PerCovCalc(b_IA_1, "barnacle_Per_Cov") # call the function
 
 # Mussels
 ms_IA_1 <- filter(IA_GOA, Common_Cat=="mussel")   # Filter out the species/entry of interest
-ms_IA <- PerCovCalc(ms_IA_1, "mussel_Per_Cov") ; ms_IA  # call the function
+ms_IA <- PerCovCalc(ms_IA_1, "mussel_Per_Cov")  # call the function
 
 # Fucus distichus
 Fd_IA_1 <- filter(IA_GOA, Species_Name=="Fucus distichus")   # Filter out the species/entry of interest
-Fd_IA <- PerCovCalc(Fd_IA_1, "Fuc_dist_Per_Cov") ; Fd_IA  # call the function
+Fd_IA <- PerCovCalc(Fd_IA_1, "Fuc_dist_Per_Cov")  # call the function
 
 # Alaria marginata
 Am_IA_1 <- filter(IA_GOA, Species_Name=="Alaria marginata")   # Filter out the species/entry of interest
-Am_IA <- PerCovCalc(Am_IA_1, "Ala_marg_Per_Cov") ; Am_IA  # call the function
+Am_IA <- PerCovCalc(Am_IA_1, "Ala_marg_Per_Cov")  # call the function
 
 # Neorhodomela sp
 Neo_sp <- c("Neorhodomela oregona","Neorhodomela larix","Odonthalia / Neorhodomela sp.","Odonthalia floccosa")
 No_IA_1 <- filter(IA_GOA, Species_Name %in% Neo_sp)   # Filter out the species/entry of interest
-No_IA <- PerCovCalc(No_IA_1, "Neo_sp_Per_Cov") ; No_IA  # call the function
+No_IA <- PerCovCalc(No_IA_1, "Neo_sp_Per_Cov") # call the function
 
 
 
@@ -147,7 +153,8 @@ No_IA <- PerCovCalc(No_IA_1, "Neo_sp_Per_Cov") ; No_IA  # call the function
 # Ephemeral Algae
 ephem_alg <- c("filamentous_brown", "filamentous_green", "green_alga")
 ephm_IA_1 <- filter(IA_GOA, Common_Cat %in% ephem_alg)   # Filter out the species/entry of interest
-ephm_IA <- PerCovCalc(ephm_IA_1, "ephemeral_algae_Per_Cov") ; ephm_IA  # call the function
+ephm_IA <- PerCovCalc(ephm_IA_1, "ephemeral_algae_Per_Cov")  # call the function
+
 
 
 
