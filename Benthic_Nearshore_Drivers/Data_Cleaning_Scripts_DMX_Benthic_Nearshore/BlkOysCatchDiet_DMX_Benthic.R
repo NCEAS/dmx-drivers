a9e5aba701a238cc6498c2c72b6ad52207e21ae1
Rachael Blake <blake@nceas.ucsb.edu>
2015-10-16 18:59:32 -0700

    added genus-level info for aggregation

--- Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/BlkOysCatchDiet_DMX_Benthic.R
+++ Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/BlkOysCatchDiet_DMX_Benthic.R
@@ -33,63 +33,140 @@ BLOYzip1 <- unzip(BLOYzipd, list=TRUE)
 BLOYzip <- BLOYzip1[grep(".csv", BLOYzip1$Name),]$Name # subsets all the .csv files
 BLOYzip_diet <- BLOYzip[grep("diet", BLOYzip)]   # subsets the diet data files
 
-
 unzip_read <- function(file_list){
               # for every .csv file in zipped file list, do the following 
               z <- unzip(BLOYzipd, file_list)
               rbind.fill(lapply(z, read.csv))
               }
 
-BLOY_diet <- unzip_read(BLOYzip_diet)
-unlink(BLOYzipd)
+BLOY_diet1 <- unzip_read(BLOYzip_diet)  ;  unlink(BLOYzipd)
 
 # Cleaning, filtering, etc. 
+# make common genus-level categories
+Nucella <- c("Nucella spp.","Nucella lima","Nucella canaliculata","N. lima","N. lamellosa")
+Lottia <- c("Lottia","Lottia persona","Lottia digitalis","Lottia scutum","Lottia pelta",
+            "L. pelta","L. digitalis","L. scutum")
+Mopalia <- c("Mopalia","Mopalia sp.","M. lignosa")
+Buccinum <- c("Buccinum baeri","B. baeri")
+Mytilus <- c("Mytilus trossulus","M. trossulus")
+Hiatella <- c("Hiatella arctica","H. arctica")
+Modiolus <- c("Modiolus modiolus","M. modiolus")
+Acmea <- c("Acmea mitra","A. mitra")
+Katharina <- c("Katharina tunicata","K. tunicata")
+Pododesmus <- c("Pododesmus sp.","P. macrochisma")
+Tectura <- c("T. scutum","T. persona")                    
+Macoma <- c("Macoma sp.")                           
+Lirabuccinum <- c("L. dirum")
+Saxidomus <- c("S. gigantea")
+Leukoma <- c("L. staminea")                           
+Telmessus <- c("T. cheiragonus")                
+Littorina <- c("L. sitkana")
+
+# N. marginata  ??????????????????????????????                             
+
 # clean first data frame (pre 2014 data)
+BLOY_diet1[BLOY_diet1 == "."] <- NA  # replace "." with NA in the entire data frame
 
+BLOY_diet <- BLOY_diet1 %>% 
+             rename(Size_mm=Size, Site=Site.) %>% 
+             select(-Comment, -Observers) %>%
+             filter(Year %in% c(2010,2011,2012,2013,2014,2015)) %>%
+             mutate(Region = replace(as.character(Region), Region=="PWS", "WPWS"),
+                    Site_Name = ifelse((Site=="RI1" & Region=="KATM"),'Kukak Bay',
+                                ifelse((Site=="RI1" & Region=="KEFJ"),'Aialik Bay',       
+                                ifelse((Site=="RI1" & Region=="WPWS"),'Hogan Bay',   
+                                ifelse((Site=="RI2" & Region=="KATM"),'Kaflia Bay',
+                                ifelse((Site=="RI2" & Region=="KEFJ"),'McCarty Fjord',
+                                ifelse((Site=="RI2" & Region=="WPWS"),'Iktua Bay',  
+                                ifelse((Site=="RI3" & Region=="KATM"),'Kinak Bay',  
+                                ifelse((Site=="RI3" & Region=="KEFJ"),'Nuka Bay',  
+                                ifelse((Site=="RI3" & Region=="WPWS"),'Whale Bay',
+                                ifelse((Site=="RI4" & Region=="KATM"),'Amalik Bay',  
+                                ifelse((Site=="RI4" & Region=="KEFJ"),'Nuka Passage',       
+                                ifelse((Site=="RI4" & Region=="WPWS"),'Johnson Bay',    
+                                ifelse((Site=="RI5" & Region=="KATM"),'Takli Island',  
+                                ifelse((Site=="RI5" & Region=="KEFJ"),'Harris Bay', 
+                                ifelse((Site=="RI5" & Region=="WPWS"),'Herring Bay',  
+                                ""))))))))))))))),
+                    Genus = ifelse((Species %in% Acmea),'Acmea',
+                            ifelse((Species %in% Buccinum),'Buccinum',
+                            ifelse((Species %in% Hiatella),'Hiatella',
+                            ifelse((Species %in% Katharina),'Katharina', 
+                            ifelse((Species %in% Leukoma),'Leukoma',  
+                            ifelse((Species %in% Lirabuccinum),'Lirabuccinum',   
+                            ifelse((Species %in% Littorina),'Littorina',
+                            ifelse((Species %in% Lottia),'Lottia',   
+                            ifelse((Species %in% Macoma),'Macoma',       
+                            ifelse((Species %in% Modiolus),'Modiolus',  
+                            ifelse((Species %in% Mopalia),'Mopalia',
+                            ifelse((Species %in% Mytilus),'Mytilus',     
+                            ifelse((Species %in% Nucella),'Nucella',
+                            ifelse((Species %in% Pododesmus),'Pododesmus',    
+                            ifelse((Species %in% Saxidomus),'Saxidomus', 
+                            ifelse((Species %in% Tectura),'Tectura', 
+                            ifelse((Species %in% Telmessus),'Telmessus', 
+                            "")))))))))))))))))       
+                    ) %>%
+             mutate_each(funs(as.numeric), Size_mm) # change class of column           
 
+# clean 2014 data 
+BLOYD[BLOYD == "."] <- NA  # replace "." with NA in the entire data frame
 
-
-
+BLOYD_D <- BLOYD %>%
+           rename(REGIONBLOCK=Region,Region=Block.Name,Size_mm=Size..mm.) %>%
+           mutate(Year = sapply(strsplit(as.character(Date), split="/") , function(x) x[3]), # create Sample Year column
+                  Year = ifelse(Year == "14", '2014', Year),  # replace weird first date 
+                  Site_Name = ifelse((Site=="Ninagiak" & Region=="KATM"),'Ninagiak Island',
+                              ifelse((Site=="RI-02" & Region=="KATM"),'Kaflia Bay',
+                              ifelse((Site=="RI-03" & Region=="KATM"),'Kinak Bay',
+                              ifelse((Site=="RI-04" & Region=="KATM"),'Amalik Bay',
+                              ifelse((Site=="RI-04" & Region=="KEFJ"),'Nuka Passage',
+                              ifelse((Site=="RI-05" & Region=="KATM"),'Takli Island',
+                              ifelse((Site=="Shakun" & Region=="KATM"),'Shakun',
+                              ifelse((Site=="RI-01" & Region=="KEFJ"),'Aialik Bay',
+                              ifelse((Site=="2" & Region=="WPWS"),'Iktua Bay',
+                              ifelse((Site=="3" & Region=="WPWS"),'Whale Bay',
+                              ifelse((Site=="5" & Region=="WPWS"),'Herring Bay', 
+                              ""))))))))))),
+                  Genus = ifelse((Species %in% Acmea),'Acmea',
+                            ifelse((Species %in% Buccinum),'Buccinum',
+                            ifelse((Species %in% Hiatella),'Hiatella',
+                            ifelse((Species %in% Katharina),'Katharina', 
+                            ifelse((Species %in% Leukoma),'Leukoma',  
+                            ifelse((Species %in% Lirabuccinum),'Lirabuccinum',   
+                            ifelse((Species %in% Littorina),'Littorina',
+                            ifelse((Species %in% Lottia),'Lottia',   
+                            ifelse((Species %in% Macoma),'Macoma',       
+                            ifelse((Species %in% Modiolus),'Modiolus',  
+                            ifelse((Species %in% Mopalia),'Mopalia',
+                            ifelse((Species %in% Mytilus),'Mytilus',     
+                            ifelse((Species %in% Nucella),'Nucella',
+                            ifelse((Species %in% Pododesmus),'Pododesmus',    
+                            ifelse((Species %in% Saxidomus),'Saxidomus', 
+                            ifelse((Species %in% Tectura),'Tectura', 
+                            ifelse((Species %in% Telmessus),'Telmessus', 
+                            "")))))))))))))))))  
+                  ) %>%
+           mutate_each(funs(as.numeric), Size_mm) # change class of column 
 
 
 # bind the two data frames together
-BLOYD2 <- rbind.fill(BLOYD, BLOY_diet)
+BLOYD_GOA <- rbind.fill(BLOYD_D, BLOY_diet)
 
-# more cleaning
-BLOYD2[BLOYD2 == "."] <- NA  # replace "." with NA in the entire data frame
-
-BLOYD_GOA <- BLOYD2 %>%
-             rename(REGIONBLOCK=Region,Region=Block.Name,Size_mm=Size..mm.) %>%
-             mutate(Year = sapply(strsplit(as.character(Date), split="/") , function(x) x[3]), # create Sample Year column
-                    Year = ifelse(Year == "14", '2014', Year),  # replace weird first date 
-                    Site_Name = ifelse((Site=="Ninagiak" & Region=="KATM"),'Ninagiak Island',
-                                ifelse((Site=="RI-02" & Region=="KATM"),'Kaflia Bay',
-                                ifelse((Site=="RI-03" & Region=="KATM"),'Kinak Bay',
-                                ifelse((Site=="RI-04" & Region=="KATM"),'Amalik Bay',
-                                ifelse((Site=="RI-04" & Region=="KEFJ"),'Nuka Passage',
-                                ifelse((Site=="RI-05" & Region=="KATM"),'Takli Island',
-                                ifelse((Site=="Shakun" & Region=="KATM"),'Shakun',
-                                ifelse((Site=="RI-01" & Region=="KEFJ"),'Aialik Bay',
-                                ifelse((Site=="2" & Region=="WPWS"),'Iktua Bay',
-                                ifelse((Site=="3" & Region=="WPWS"),'Whale Bay',
-                                ifelse((Site=="5" & Region=="WPWS"),'Herring Bay', 
-                                "")))))))))))       
-                    ) %>%
-             mutate_each(funs(as.numeric), Size_mm) # change class of column 
 
 BLOYD_Abun <- BLOYD_GOA %>%
-              count(Region,Year,Site_Name,Nest.site..,Species) %>%
+              count(Region,Year,Site_Name,Genus) %>%
               rename(BLOYDietItem_Abun=n)
   
 BLOYD_Size <- BLOYD_GOA %>%
-              group_by(Region,Year,Site_Name,Nest.site..,Species) %>%
+              group_by(Region,Year,Site_Name,Genus) %>%
               summarize(BLOYDietItem_meanSzmm = mean(Size_mm, na.rm=TRUE)) %>%
               ungroup()
 
 # still need to add calculation of the proportion of biomass provided by 
 # limpets, mussels, other                      
                       
-                      
+                    
                       
                       
                       
