df8d17210c1b4e96b63389b660e37fefe3b0d629
Rachael Blake <blake@nceas.ucsb.edu>
2016-04-01 13:20:36 -0700

    added seasonal pdo breakdown

--- Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/PDO_DMX_Benthic.R
+++ Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/PDO_DMX_Benthic.R
@@ -30,11 +30,41 @@ pdo_df <- read.table(file=textConnection(pdo_pre), skip=30, nrows=116, stringsAs
                   header=FALSE, col.names=pdo_cols, strip.white=TRUE, fill=TRUE)
 pdo_df$YEAR <- substr(pdo_df$YEAR, 1, 4)  # removes asterisks from years 2002-2015
 #
-pdo_annual <- pdo_df %>% 
-              rename(Year=YEAR) %>% # rename data columns         
-      #        filter(Year %in% c(2010:2015)) %>% # selects years 
-              gather(Month, PDO, -Year) %>% # reshapes data to be column-wise
-              group_by(Year) %>%
-              summarise(PDO_anul_mn=mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get annual means
-              ungroup() 
+pdo_ann <- pdo_df %>% 
+           rename(Year=YEAR) %>% # rename data columns         
+      #    filter(Year %in% c(2010:2015)) %>% # selects years 
+           gather(Month, PDO, -Year) %>% # reshapes data to be column-wise
+           group_by(Year) %>%
+           summarise(PDO_anul_mn=mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get annual means
+           ungroup() 
 
+# Spring PDO March through May 
+pdo_spr <- pdo_df %>% 
+           rename(Year=YEAR) %>% # rename data columns         
+      #    filter(Year %in% c(2010:2015)) %>% # selects years 
+           gather(Month, PDO, -Year) %>% # reshapes data to be column-wise
+           filter(Month %in% c("MAR","APR","MAY")) %>%
+           group_by(Year) %>%
+           summarise(PDO_spring_mn=mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get annual means
+           ungroup() 
+
+# Winter PDO Dec through Feb
+pdo_win <- pdo_df %>% 
+           rename(Year=YEAR) %>% # rename data columns         
+      #    filter(Year %in% c(2010:2015)) %>% # selects years 
+           gather(Month, PDO, -Year) %>% # reshapes data to be column-wise
+           filter(Month %in% c("JAN","FEB","DEC")) %>%
+           group_by(Year) %>%
+           summarise(PDO_winter_mn=mean(as.numeric(as.character(PDO)), na.rm = TRUE)) %>% # get annual means
+           ungroup() 
+
+
+PDO_ann_spr <- merge(pdo_ann,pdo_spr,all.x=T)
+PDO_ann_seasn <- merge(PDO_ann_spr,pdo_win,all.x=T)  
+  
+  
+  
+  
+  
+  
+  
