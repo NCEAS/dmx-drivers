e4872dd244734df8307509b4369aa065475f6cb5
Rachael Blake <blake@nceas.ucsb.edu>
2016-11-04 07:32:05 -0700

    cleaning Hobo Water Temp data

--- Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/HoboWaterTemp_DMX_Benthic.R
+++ Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/HoboWaterTemp_DMX_Benthic.R
@@ -26,3 +26,87 @@ HWT <- read.table(file=textConnection(HWT1), sep="\t", header=TRUE)
 head(HWT)
 
 
+HoboWTmp <- HWT %>%
+            dplyr::rename(Region=block, Year=year) %>%
+            separate(col=sensor, into=c("Site_Name", "Elevation"), sep="0") %>%
+            select(-Elevation) %>%
+            mutate(Site_Name = ifelse(Site_Name=="Kukak_",'Kukak Bay',
+                               ifelse(Site_Name=="Aialik_",'Aialik Bay',       
+                               ifelse(Site_Name=="Hogan_",'Hogan Bay',   
+                               ifelse(Site_Name=="Kaflia_",'Kaflia Bay',
+                               ifelse(Site_Name=="McCarty_",'McCarty Fjord',
+                               ifelse(Site_Name=="Iktua_",'Iktua Bay',  
+                               ifelse(Site_Name=="Kinak_",'Kinak Bay',  
+                               ifelse(Site_Name=="Nuka_Bay_",'Nuka Bay',  
+                               ifelse(Site_Name=="Whale_",'Whale Bay',
+                               ifelse(Site_Name=="Amalik_",'Amalik Bay',  
+                               ifelse(Site_Name=="Nuka_Pass_",'Nuka Passage',       
+                               ifelse(Site_Name=="Johnson_",'Johnson Bay',    
+                               ifelse(Site_Name=="Takli_",'Takli Island',  
+                               ifelse(Site_Name=="Harris_",'Harris Bay', 
+                               ifelse(Site_Name=="Herring_",'Herring Bay',   
+                               "")))))))))))))))
+                   ) 
+
+
+
+Hobo_WTmp_ann <- HoboWTmp %>%
+                 select(-X_TYPE_, -X_FREQ_, -std_w_temp) %>%
+                 group_by(Region, Site_Name, Year) %>%
+                 summarise(Hobo_WaterTemp_AnnMn=mean(mean_w_temp),
+                           Hobo_WaterTemp_AnnMin=mean(min_w_temp),
+                           Hobo_WaterTemp_AnnMax=mean(max_w_temp)) %>%
+                 ungroup() %>%
+                 arrange(Site_Name, Year)
+
+
+# Examine data for holes in the data
+melt_WTmp <- reshape::melt.data.frame(as.data.frame(Hobo_WTmp_ann), id.vars=c("Year","Region","Site_Name"),      
+                      variable_name="WTmp_type")
+melt_WTmp <- arrange(melt_WTmp, Site_Name, Year)
+
+# 15 sites, 3 data types, 10 years = 450 rows that the full dataframe should have
+
+all_yrs <- data.frame(Year = rep(c(2006,2006,2006,2007,2007,2007,2008,2008,2008,2009,2009,2009,
+                                   2010,2010,2010,2011,2011,2011,2012,2012,2012,2013,2013,2013,
+                                   2014,2014,2014,2015,2015,2015), 15),
+                      Region = rep(c("WPWS","KEFJ","KATM"), 15))
+
+all_info <- all_yrs[rep(seq_len(nrow(all_yrs)), each=5),]
+
+
+m_WTmp <- all_info %>%
+          full_join(Hobo_WTmp_ann) %>%
+          select(-Hobo_WaterTemp_AnnMin, -Hobo_WaterTemp_AnnMax) 
+
+
+              mutate(Bin_Value = ifelse((is.na(value)),'0',
+                                 ifelse((!is.na(value)),'1',""))) %>%
+             # dplyr::rename(DataSet=variable) %>%
+              mutate_each(funs(as.character), Site_Name) %>%
+              mutate_each(funs(as.character), Data_Set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
