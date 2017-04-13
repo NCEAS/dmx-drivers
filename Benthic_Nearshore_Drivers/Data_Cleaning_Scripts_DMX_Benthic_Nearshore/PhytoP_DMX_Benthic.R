4d4216c694b6e18acc982fee9a3bd225c514d61d
Rachael Blake <blake@nceas.ucsb.edu>
2017-04-13 11:57:13 -0700

    revised column re-naming and rebuilt MusselQuest csv

--- Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/PhytoP_DMX_Benthic.R
+++ Benthic_Nearshore_Drivers/Data_Cleaning_Scripts_DMX_Benthic_Nearshore/PhytoP_DMX_Benthic.R
@@ -91,6 +91,7 @@ list_csvs <- load_my_data("./Seward_Line_Chla")
 # Clean column names, etc.in the csvs
 colnames(list_csvs[[1]])[3] <- "chlorophyllA" 
 colnames(list_csvs[[1]])[4] <- "phaeophytin" 
+
 colnames(list_csvs[[2]])[3] <- "chlorophyllA" 
 colnames(list_csvs[[2]])[4] <- "phaeophytin" 
 
@@ -104,22 +105,30 @@ colnames(list_csvs[[5]])[4] <- "chlorophyllA"
 colnames(list_csvs[[5]])[5] <- "phaeophytin"
 
 list_csvs[[6]] <- list_csvs[[6]][,c(1:7,11)]
-  
-  #dplyr::select(list_csvs[[6]], -c(X.20_Chl_A..µg.L..1, Total_Chl_A..µg.L.,
-  #                                                 Total_Phaeo..µg.L.))
 colnames(list_csvs[[6]])[7] <- "chlorophyllA"
 
 colnames(list_csvs[[7]])[7] <- "chlorophyllA"
+
 colnames(list_csvs[[8]])[7] <- "chlorophyllA"
+
 colnames(list_csvs[[9]])[4] <- "chlorophyllA"
 colnames(list_csvs[[9]])[5] <- "phaeophytin"
+
 colnames(list_csvs[[10]])[7] <- "chlorophyllA"
-colnames(list_csvs[[11]])[1] <- "CTD_Cast"
-colnames(list_csvs[[11]])[2] <- "Station_Name"
-colnames(list_csvs[[11]])[3] <- "lat"
-colnames(list_csvs[[11]])[4] <- "lon"
-colnames(list_csvs[[11]])[7] <- "Depth_m"
-colnames(list_csvs[[11]])[9] <- "chlorophyllA"
+
+colnames(list_csvs[[11]])[2] <- "CTD_Cast"
+colnames(list_csvs[[11]])[9] <- "lat"
+colnames(list_csvs[[11]])[10] <- "lon"
+colnames(list_csvs[[11]])[11] <- "Depth_m"
+colnames(list_csvs[[11]])[16] <- "chlorophyllA"
+colnames(list_csvs[[11]])[17] <- "phaeophytin"
+
+colnames(list_csvs[[12]])[1] <- "CTD_Cast"
+colnames(list_csvs[[12]])[2] <- "Station_Name"
+colnames(list_csvs[[12]])[3] <- "lat"
+colnames(list_csvs[[12]])[4] <- "lon"
+colnames(list_csvs[[12]])[7] <- "Depth_m"
+colnames(list_csvs[[12]])[9] <- "chlorophyllA"
 
 # Merge all the other csvs into one dataframe
 one_df1 <- merge_recurse(list_csvs)
@@ -136,7 +145,7 @@ Chl_ALL <- merge(Chl_df, one_df, all=T,
 # DF of GAK1 sites
 Chl_GAK1 <- Chl_ALL %>%
             mutate_each(funs(toupper), Station_Name) %>%  # capitalize station names
-            filter(Station_Name %in% c("GAK 1", "GAK1", "GAK01")) %>%  # select GAK 1 only  #, "GAK01I", "GAK 1I", "GAK1I"
+            filter(Station_Name %in% c("GAK 1", "GAK1", "GAK01", " GAK1")) %>%  # select GAK 1 only  #, "GAK01I", "GAK 1I", "GAK1I"
             arrange(Datef) %>%     
             mutate(Year=substring(Datef,1,4),
                    Month=substring(Datef,6,7)) %>% 
